9447218d6c753f52deb94b7ddb9dc8c90bb5c200
Simon Bates <simon@bitstructures.com>
2015-03-31 17:05:16 -0400

    Change the binding for helm-buffers-list to C-x b

--- settings/init-helm.el
+++ settings/init-helm.el
@@ -1,7 +1,7 @@
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (helm-mode 1)
 
 (provide 'init-helm)
