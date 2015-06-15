6c0972e69451d0132df8c203bd26775206a180c0
Simon Bates <simon@bitstructures.com>
2015-06-15 17:37:27 -0400

    Avoid the "GREP_OPTIONS is deprecated" warning

--- configs/work.el
+++ configs/work.el
@@ -1,2 +1,5 @@
 (set-face-attribute 'default nil :font "DejaVu Sans Mono-10")
 (setenv "PATH" (concat "C:\\cygwin\\bin;" (getenv "PATH")))
+;; avoid the "GREP_OPTIONS is deprecated" warning
+(require 'grep)
+(grep-apply-setting 'grep-highlight-matches nil)
